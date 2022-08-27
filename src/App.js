6583b541a8a46250912496341570c84377525de9
shubham.carpenter <carpenter.shubh@gmail.com>
2022-08-27 11:38:19 +0530

    Added logo, auth layout component and register page

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// routes
+import Router from './routes';
+import './assets/css/fonts.css';
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <Router />
+  );
+}
