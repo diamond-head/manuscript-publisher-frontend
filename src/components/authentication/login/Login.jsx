c9c1571fc50aae940cf3a1ca8e38662c20d82874
shubham.carpenter <carpenter.shubh@gmail.com>
2022-09-09 18:32:02 +0530

    login component added

---
+++ src/components/authentication/login/Login.jsx
@@ -0,0 +1,112 @@
+import * as Yup from 'yup';
+import { useEffect, useState } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { Icon } from '@iconify/react';
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+// material
+import {
+  Link,
+  Stack,
+  TextField,
+  IconButton,
+  InputAdornment
+} from '@material-ui/core';
+import { LoadingButton } from '@material-ui/lab';
+import { axiosInstance } from '../../../axios/base.axios';
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    password: Yup.string().required('Password is required')
+  });
+
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (data) => {
+      axiosInstance
+        .post('/auth/login', data)
+        .then((res) => res.data)
+        .then((resp) => {
+          localStorage.setItem('auth', resp?.data?.token);
+          formik.setSubmitting(false);
+          navigate('/dashboard', { replace: true });
+        })
+        .catch((err) => {
+          formik.setSubmitting(false);
+        });
+    }
+  });
+
+  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+        </Stack>
+
+        <Stack direction="row" alignItems="center" justifyContent="space-end" sx={{ my: 2 }}>
+          <Link component={RouterLink} variant="subtitle2" to="/forgot">
+            Forgot password?
+          </Link>
+        </Stack>
+
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Login
+        </LoadingButton>
+      </Form>
+    </FormikProvider>
+  );
+}
