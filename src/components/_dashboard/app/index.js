044ad10bd2b0970c14d0d3e28c53ab879937d227
shubham.carpenter <carpenter.shubh@gmail.com>
2022-09-15 11:49:52 +0530

    some more dashboard components added

--- src/components/_dashboard/app/index.js
+++ src/components/_dashboard/app/index.js
@@ -1,2 +1,4 @@
 export { default as AppNewUsers } from './AppNewUsers'
 export { default as AppTasks } from './AppTasks'
+export { default as AppNewsUpdate } from './AppNewsUpdate'
+export { default as AppOrderTimeline } from './AppOrderTimeline'
